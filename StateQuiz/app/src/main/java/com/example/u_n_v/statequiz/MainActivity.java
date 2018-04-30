70bffd0c8ffda0f5ddba03def5889c0f3c82cf95
Aaron <uenvymenow@gmail.com>
2018-04-30 18:55:59 -0500

    All questions are added with correct answers. Scores are calculated appropriately. Buttons submit or reset. TODO: 1) Add incorrect answers to questions 2) Make sure that all code is either used or deleted. 3) Make sure that text in layout is formatted in strings.xml 4) Format code 5) Format spacing of app 6) Add a header image with U.S. flag waving

--- StateQuiz/app/src/main/java/com/example/u_n_v/statequiz/MainActivity.java
+++ StateQuiz/app/src/main/java/com/example/u_n_v/statequiz/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -79,6 +80,97 @@ public class MainActivity extends AppCompatActivity {
         return question3Score;
     }
 
+    // Grabs the question4RadioButton and sees if it is checked or not for the correct answer
+    public void question4RadioButton(View view) {
+        // Import radio buttons for question 4
+        RadioButton questionFourCorrectRB = findViewById(R.id.question4CorrectAnswer);
+
+        // Check if correct RadioButton is clicked
+        if (questionFourCorrectRB.isChecked()) {
+            question4Score = 1;
+        } else {
+            question4Score = 0;
+        }
+    }
+
+    // Grabs the question5RadioButton and sees if it is checked or not for the correct answer
+    public void question5RadioButton(View view) {
+        // Import radio buttons for question 5
+        RadioButton questionFiveCorrectRB = findViewById(R.id.question5CorrectAnswer);
+
+        // Check if correct RadioButton is clicked
+        if (questionFiveCorrectRB.isChecked()) {
+            question5Score = 1;
+        } else {
+            question5Score = 0;
+        }
+    }
+
+    // Grabs the question6RadioButton and sees if it is checked or not for the correct answer
+    public void question6RadioButton(View view) {
+        // Import radio buttons for question 6
+        RadioButton questionSixCorrectRB = findViewById(R.id.question6CorrectAnswer);
+
+        // Check if correct RadioButton is clicked
+        if (questionSixCorrectRB.isChecked()) {
+            question6Score = 1;
+        } else {
+            question6Score = 0;
+        }
+    }
+
+    // Grabs the question7RadioButton and sees if it is checked or not for the correct answer
+    public void question7RadioButton(View view) {
+        // Import radio buttons for question 7
+        RadioButton questionSevenCorrectRB = findViewById(R.id.question7CorrectAnswer);
+
+        // Check if correct RadioButton is clicked
+        if (questionSevenCorrectRB.isChecked()) {
+            question7Score = 1;
+        } else {
+            question7Score = 0;
+        }
+    }
+
+    // Grabs the question8RadioButton and sees if it is checked or not for the correct answer
+    public void question8RadioButton(View view) {
+        // Import radio buttons for question 8
+        RadioButton questionEightCorrectRB = findViewById(R.id.question8CorrectAnswer);
+
+        // Check if correct RadioButton is clicked
+        if (questionEightCorrectRB.isChecked()) {
+            question8Score = 1;
+        } else {
+            question8Score = 0;
+        }
+    }
+
+    // Grabs the question9 correct answer check boxes and sees if they are checked or not
+    public void question9CB(View v){
+        CheckBox correctAnswer1 = findViewById(R.id.question9CorrectOne);
+        CheckBox correctAnswer2 = findViewById(R.id.question9CorrectTwo);
+
+        if (correctAnswer1.isChecked() && correctAnswer2.isChecked()){
+            question9Score = 1;
+        }
+        else {
+            question9Score = 0;
+        }
+    }
+
+    // Grabs the question10RadioButton and sees if it is checked or not for the correct answer
+    public void question10RadioButton(View view) {
+        // Import radio buttons for question 10
+        RadioButton questionTenCorrectRB = findViewById(R.id.question10CorrectAnswer);
+
+        // Check if correct RadioButton is clicked
+        if (questionTenCorrectRB.isChecked()) {
+            question10Score = 1;
+        } else {
+            question10Score = 0;
+        }
+    }
+
     // Displays Toast Message of final score
     public void displayScore(){
         Toast.makeText(this, "Your final score is " + calculateFinalScore(calculateCorrectScore(), totalQuestions) + "%", Toast.LENGTH_SHORT).show();
@@ -95,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
         question3Score = question3EditText();
 
         // Adds all of the correct answers
-        correctAnswers = question1Score + question2Score + question3Score;
+        correctAnswers = question1Score + question2Score + question3Score + question4Score + question5Score + question6Score + question7Score + question8Score + question9Score + question10Score;
         return correctAnswers;
     }
 
@@ -103,5 +195,12 @@ public class MainActivity extends AppCompatActivity {
     public int calculateFinalScore(int answersCorrect, int questionTotal) {
         int percentCorrect = (int) (((double) answersCorrect / (double) questionTotal) * 100);
         return percentCorrect;
+    }
+
+    // Resets the scores for each question and the final score
+    public void resetButton(View view){
+        Intent resetApp = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+        resetApp.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(resetApp);
     }
 }

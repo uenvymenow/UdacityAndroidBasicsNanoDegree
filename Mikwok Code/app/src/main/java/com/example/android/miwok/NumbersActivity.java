0de795267f02d88db0978eb04b95c920b209e7d1
Aaron <uenvymenow@gmail.com>
2018-05-09 23:15:41 -0500

    Reverted changes back to using a ListView after experimenting with GridViews with ArrayAdapters

--- Mikwok Code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Mikwok Code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
